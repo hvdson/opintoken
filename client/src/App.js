f75b0d97f562f7c432127d1a7fb6dc0fb4845507
hvdson <hvdsonmusic@gmail.com>
2021-01-09 15:23:23 -0800

    finished initializing everything

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = SimpleStorageContract.networks[networkId];
@@ -26,6 +25,7 @@ class App extends Component {
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
+      console.log(web3, accounts, instance)
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
